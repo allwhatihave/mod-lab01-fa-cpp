fa5df41813dca4629de6754d3afd69a3754cc109
allwhatihave <kirillit2016tig@gmail.com>
2026-03-29 18:22:06 +0300

    изменил мейн

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
-// Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char* testStr = "Hello world 123 hello1 Cplusplus KirillTig";
+    
+    std::cout << "String: " << testStr << std::endl;
+    std::cout << "Task 1: " << faStr1(testStr) << std::endl;
+    std::cout << "Task 2: " << faStr2(testStr) << std::endl;
+    std::cout << "Task 3: " << faStr3(testStr) << std::endl;
+
     return 0;
 }
